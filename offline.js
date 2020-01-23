97db7835d02fb7a3b04b5f1d449b613b0ad446ae
morvy666 <morvy0@gmail.com>
2020-01-23 13:19:28 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1579794840,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/uibg.png",
+		"images/influencearea.png",
+		"images/rollindex-sheet0.png",
+		"images/slidebg.png",
+		"images/slidebutton-sheet0.png",
+		"images/questbg-sheet0.png",
+		"images/btn_blue_circle-sheet0.png",
+		"images/btn_blue_circle-sheet1.png",
+		"images/btn_green_circle-sheet0.png",
+		"images/btn_green_circle-sheet1.png",
+		"images/btn_red_circle-sheet0.png",
+		"images/btn_red_circle-sheet1.png",
+		"images/slidecontrol-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
